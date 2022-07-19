09afba0768ad9fef0cf40d08535c357405599289
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-19 13:00:08 +0800

    > compile NEMU ysyx_22041461 陈星宇 Linux cxy-Lenovo-XiaoXinPro-13IML-2020 5.15.0-40-generic #43-Ubuntu SMP Wed Jun 15 12:54:21 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:00:08 up  2:50,  1 user,  load average: 1.06, 0.84, 0.57

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -54,6 +54,14 @@ static int cmd_q(char *args) {
 
 static int cmd_help(char *args);
 
+static int cmd_si(char *args) {
+  int step;
+  if(args == NULL){ step = 1; }
+  else sscanf(args,"%d",&step);
+  cpu_exec(step);
+  return 0;
+}
+
 static struct {
   const char *name;
   const char *description;
@@ -62,6 +70,7 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
+  { "si", "Single step execution", cmd_si },
 
   /* TODO: Add more commands */
 
