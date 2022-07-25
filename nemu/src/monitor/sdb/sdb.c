45476327ee4207905fa1cda30e370a6a4687fb59
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-25 16:46:09 +0800

    > compile NEMU ysyx_22041461 陈星宇 Linux cxy-lenovo 5.15.0-41-generic #44-Ubuntu SMP Wed Jun 22 14:20:53 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:46:09 up  6:03,  1 user,  load average: 0.49, 0.70, 0.81

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -95,7 +95,7 @@ static int cmd_x(char *args){
 static int cmd_p(char *args){
   if(make_token(args) == true){
     extern int nr_token;
-    int num = eval(1, nr_token);
+    int num = eval(0, nr_token);
     printf("%s = %d", args, num);
   }
   return 0;
