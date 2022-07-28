b9249cab1fa938ef9532108b6bc59f70db166530
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-28 23:35:20 +0800

    > compile NEMU ysyx_22041461 陈星宇 Linux cxy-lenovo 5.15.0-41-generic #44-Ubuntu SMP Wed Jun 22 14:20:53 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:35:20 up  7:16,  1 user,  load average: 1.11, 0.34, 0.41

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -39,7 +39,6 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
 #endif
   if (g_print_step) { IFDEF(CONFIG_ITRACE, puts(_this->logbuf)); }
   IFDEF(CONFIG_DIFFTEST, difftest_step(_this->pc, dnpc));
-#ifdef CONFIG_WATCHPOINT
   int i;
   extern WP *HEAD;
   for(i = 0; i < NR_WP; i++){
@@ -54,7 +53,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc) {
       }
     }
   }
-#endif
+
 }
 
 static void exec_once(Decode *s, vaddr_t pc) {
