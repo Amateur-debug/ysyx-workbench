5699b5899ec1c12b3f00a289a65e8fb52f577810
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-28 17:43:43 +0800

    > compile NEMU ysyx_22041461 陈星宇 Linux cxy-lenovo 5.15.0-41-generic #44-Ubuntu SMP Wed Jun 22 14:20:53 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:43:43 up  1:24,  1 user,  load average: 0.55, 0.92, 0.95

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -77,7 +77,7 @@ static int cmd_info(char *args){
         break;
       }
       else{
-        printf("监视点%d    %s\n", b->NO, b->experence);
+        printf("监视点%d\t%s\n", b->NO, b->experence);
         b = b->next;
       }
     }
