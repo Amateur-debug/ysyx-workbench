f0e2e53cbde0946ee2bac630429365eaa7e10e24
tracer-ysyx2204 <tracer@ysyx.org>
2022-07-30 19:27:14 +0800

    > compile NEMU ysyx_22041461 陈星宇 Linux cxy-lenovo 5.15.0-41-generic #44-Ubuntu SMP Wed Jun 22 14:20:53 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  19:27:14 up  9:17,  1 user,  load average: 1.83, 1.60, 0.99

--- nemu/src/utils/state.c
+++ nemu/src/utils/state.c
@@ -20,5 +20,5 @@ NEMUState nemu_state = { .state = NEMU_STOP };
 int is_exit_status_bad() {
   int good = (nemu_state.state == NEMU_END) ||
     (nemu_state.state == NEMU_QUIT);
-  return good;
+  return !good;
 }
