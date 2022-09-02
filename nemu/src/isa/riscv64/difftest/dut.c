6cb8ac32abe61c38052d19e88a5e644b80fc6959
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-02 14:19:45 +0800

    > compile NEMU ysyx_22041461 陈星宇 Linux cxy-lenovo 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:19:45 up 1 day,  3:36,  1 user,  load average: 1.38, 1.23, 1.10

--- nemu/src/isa/riscv64/difftest/dut.c
+++ nemu/src/isa/riscv64/difftest/dut.c
@@ -20,6 +20,9 @@
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   int i;
   extern CPU_state cpu;
+  if(cpu.pc != pc){
+    return false;
+  }
   for(i = 0; i < 32; i++){
     if(cpu.gpr[i] != ref_r->gpr[i]){
       return false;
