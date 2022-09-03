9cfc93774faba0f1d020df0c811372e335f594b1
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-03 15:06:15 +0800

    > compile NEMU ysyx_22041461 陈星宇 Linux cxy-lenovo 5.15.0-46-generic #49-Ubuntu SMP Thu Aug 4 18:03:25 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:06:15 up  4:08,  1 user,  load average: 1.25, 1.14, 1.07

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -22,11 +22,11 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   uint32_t *pixels = ctl->pixels;
-  int i, x, y, w, h;
+  int i, x, y;
   x = ctl->x;
   y = ctl->y;
-  w = ctl->w;
-  h = ctl->h;
+  //w = ctl->w;
+  //h = ctl->h;
   /*uint64_t addr = FB_ADDR + x * 4 + y * inw(VGACTL_ADDR) * 4;
   for(j = 0; j < h; j++){
     addr += j * inw(VGACTL_ADDR) * 4;
@@ -36,7 +36,7 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
     }
   }*/
   uint64_t addr = FB_ADDR + x * 4 + y * inw(VGACTL_ADDR) * 4;
-  for(i = 0; i < w * h; i++){
+  for(i = 0; i < sizeof(pixels); i++){
       addr += i * 4;
       outl(addr, *pixels);
   }
