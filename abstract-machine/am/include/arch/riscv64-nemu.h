1d54bf6a2edf8ce95b8891e0ea9e71b42279ad0e
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-06 22:16:53 +0800

    > compile NEMU ysyx_22041461 陈星宇 Linux cxy-lenovo 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:16:53 up 1 day,  4:35,  1 user,  load average: 0.92, 1.05, 1.22

--- abstract-machine/am/include/arch/riscv64-nemu.h
+++ abstract-machine/am/include/arch/riscv64-nemu.h
@@ -4,8 +4,9 @@
 struct Context {
   // TODO: fix the order of these members to match trap.S
   //uintptr_t  gpr[32], mcause, mstatus, mepc;
-  uintptr_t mepc, mstatus, mcause, gpr[32];
   void *pdir;
+  uintptr_t mepc, mstatus, mcause, gpr[32];
+  //void *pdir;
 };
 
 #define GPR1 gpr[17] // a7
