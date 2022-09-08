a51f57c454755ae3cfc20bf29f905ae8a4b37d92
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-08 23:45:52 +0800

    > compile NEMU ysyx_22041461 陈星宇 Linux cxy-lenovo 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:45:52 up  5:46,  1 user,  load average: 1.48, 1.30, 1.18

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -4,7 +4,7 @@
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case EVENT_YIELD: printf("handled event ID = %d\n", e.event); c->mepc += 4; break;
-    case EVENT_SYSCALL: break;
+    case EVENT_SYSCALL: printf("handled event ID = %d\n", e.event);  c->mepc += 4; break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
