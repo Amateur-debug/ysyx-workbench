7c87060abb73d8c37f65dbb343094a93a2ca7b81
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-08 22:50:04 +0800

    > compile NEMU ysyx_22041461 陈星宇 Linux cxy-lenovo 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:50:04 up  4:50,  1 user,  load average: 2.43, 2.13, 1.68

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -28,7 +28,7 @@ word_t RCSR(int csr_num){
     case 0x341: return cpu.mepc;
     case 0x342: return cpu.mcause;
     case 0x300: return cpu.mstatus;
-    default: return 0;
+    default: assert(0);
   }
   return 0;
 }
@@ -39,7 +39,7 @@ void WCSR(int csr_num, uint64_t wdata){
     case 0x341: cpu.mepc = wdata; break;
     case 0x342: cpu.mcause = wdata; break;
     case 0x300: cpu.mstatus = wdata; break;
-    default: return;
+    default: assert(0);
   }
 }
 
