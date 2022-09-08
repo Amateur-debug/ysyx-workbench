8ea58d1a53bf38610106a48360f86847693f11b0
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-08 23:00:51 +0800

    > compile NEMU ysyx_22041461 陈星宇 Linux cxy-lenovo 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  23:00:51 up  5:01,  1 user,  load average: 0.67, 1.17, 1.38

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -61,14 +61,14 @@ static word_t immS(uint32_t i) { return SEXT(BITS(i, 31, 25), 7) << 5 | BITS(i, 
 static word_t immJ(uint32_t i) { return SEXT(BITS(i, 31, 31), 1) << 20 | BITS(i, 19, 12) << 12 | BITS(i, 20, 20) << 11 | BITS(i, 30, 21) << 1;}
 static word_t immB(uint32_t i) { return SEXT(BITS(i, 31, 31), 1) << 12 | BITS(i, 7, 7) << 11 | BITS(i, 30, 25) << 5 | BITS(i, 11, 8) << 1;}
 
-static void decode_operand(Decode *s, word_t *dest, word_t *src1, word_t *src2, word_t *csr, int type) {
+static void decode_operand(Decode *s, word_t *dest, word_t *src1, word_t *src2, int type) {
   uint32_t i = s->isa.inst.val;
   int rd  = BITS(i, 11, 7);
   int rs1 = BITS(i, 19, 15);
   int rs2 = BITS(i, 24, 20);
   destR(rd);
   switch (type) {
-    case TYPE_I: src1R(rs1); src2I(immI(i)); *csr = RCSR(*src2); break;
+    case TYPE_I: src1R(rs1); src2I(immI(i)); break;
     case TYPE_U: src1I(immU(i)); break;
     case TYPE_S: destI(immS(i)); src1R(rs1); src2R(rs2); break;
     case TYPE_J: src1I(immJ(i)); break;
@@ -83,7 +83,7 @@ static int decode_exec(Decode *s) {
 
 #define INSTPAT_INST(s) ((s)->isa.inst.val)
 #define INSTPAT_MATCH(s, name, type, ... /* body */ ) { \
-  decode_operand(s, &dest, &src1, &src2, &csr, concat(TYPE_, type)); \
+  decode_operand(s, &dest, &src1, &src2, concat(TYPE_, type)); \
   __VA_ARGS__ ; \
 }
 
@@ -166,9 +166,9 @@ static int decode_exec(Decode *s) {
  
   INSTPAT("0000001 ????? ????? 100 ????? 01100 11", div    , R, R(dest) = (int64_t)src1 / (int64_t)src2);
   INSTPAT("??????? ????? ????? 110 ????? 00000 11", lwu    , I, R(dest) = Mr(src1 +src2, 4));
-  INSTPAT("??????? ????? ????? 001 ????? 11100 11", csrrw  , I, WCSR(src2, src1); R(dest) = csr);
+  INSTPAT("??????? ????? ????? 001 ????? 11100 11", csrrw  , I, csr = RCSR(BITS(src2, 11, 0)); WCSR(BITS(src2, 11, 0), src1); R(dest) = csr);
 
-  INSTPAT("??????? ????? ????? 010 ????? 11100 11", csrrs  , I, WCSR(src2, src1 | csr); R(dest) = csr);
+  INSTPAT("??????? ????? ????? 010 ????? 11100 11", csrrs  , I, csr = RCSR(BITS(src2, 11, 0)); WCSR(BITS(src2, 11, 0), src1 | csr); R(dest) = csr);
   INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , I, s->dnpc = isa_raise_intr(11, s->pc));
   INSTPAT("0011000 00010 00000 000 00000 11100 11", mret   , R, s->dnpc = cpu.mepc);
 
