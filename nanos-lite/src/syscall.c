cc1929dd921f7392909b178b86cd012597b4a4b8
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-09 13:59:02 +0800

    > compile NEMU ysyx_22041461 陈星宇 Linux cxy-lenovo 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:59:02 up 19:59,  1 user,  load average: 1.16, 1.14, 1.16

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -10,7 +10,20 @@ void do_syscall(Context *c) {
   switch (a[0]) {
     case SYS_yield: yield(); c->GPRx = 0; break;
     case SYS_exit: halt(0); break;
+
     case SYS_write: 
+    if(a[1] == 1 || a[1] == 2){
+      int i;
+      for(i = 0; i < a[3]; i ++){
+        putch(*(char *)a[2]);
+      }
+      c->GPRx = i;
+    }
+    else{
+      c->GPRx = -1;
+    }
+    break;
+    
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 }
