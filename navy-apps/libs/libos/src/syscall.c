021c67382bbf7a15929308ea4ac0120b6a62812c
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-09 15:52:24 +0800

    > compile NEMU ysyx_22041461 陈星宇 Linux cxy-lenovo 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:52:24 up 21:52,  1 user,  load average: 1.05, 1.35, 1.14

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -68,7 +68,7 @@ int _write(int fd, void *buf, size_t count) {
 void *_sbrk(intptr_t increment) {
   extern char _end;
   char ret = _end;
-  if(_syscall_(SYS_brk, end + increment, 0, 0) == 0){
+  if(_syscall_(SYS_brk, _end + increment, 0, 0) == 0){
     _end = _end + increment; 
     return (void *)ret;
   }
