c43ab1fc7c200876553f24ee284bcf2205021b87
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-10 00:19:41 +0800

    > compile NEMU ysyx_22041461 陈星宇 Linux cxy-lenovo 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:19:41 up  5:00,  1 user,  load average: 1.93, 1.19, 0.83

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -80,7 +80,7 @@ long fs_write(int fd, const void *buf, size_t len){
   }
   else if(fd >= 3 && fd < fs_size){
     if(len <= file_table[fd].size){
-      i = ramdisk_write(buf, file_table[fd].disk_offset, len);
+      i = ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
       file_table[fd].open_offset += i;
     }
     else{
