1a1cc5b188c0b26142889f144db499727c2265c2
ysyx_22041461-Chen Xingyu <1046570502@qq.com>
2022-09-09 11:44:09 +0800

    系统调用完成！

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -9,14 +9,28 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+size_t ramdisk_read(void *buf, size_t offset, size_t len);
+
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  TODO();
-  return 0;
+  Elf_Ehdr ehdr;
+  ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
+  Elf_Phdr phdr[ehdr.e_phnum];
+  ramdisk_read(phdr, ehdr.e_phoff, ehdr.e_phnum * ehdr.e_phentsize);
+  int i;
+  for(i = 0; i < ehdr.e_phnum; i++){
+    if(phdr[i].p_type == PT_LOAD){
+      ramdisk_read((void *)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_memsz);
+      printf("%d %d\n", phdr[i].p_filesz, phdr[i].p_memsz);
+      memset((void *)(phdr[i].p_vaddr + phdr[i].p_filesz), 0, phdr[i].p_memsz - phdr[i].p_filesz); 
+    }
+  }
+  printf("%x\n", ehdr.e_entry);
+  return ehdr.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
   uintptr_t entry = loader(pcb, filename);
-  Log("Jump to entry = %p", entry);
+  Log("Jump to entry = %x", entry);
   ((void(*)())entry) ();
 }
 
