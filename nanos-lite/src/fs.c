a7cf715df46d6b68b5d6e71dc4cee9abe1405a45
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-10 18:41:36 +0800

    > compile NEMU ysyx_22041461 陈星宇 Linux cxy-lenovo 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:41:36 up 23:22,  1 user,  load average: 1.50, 1.21, 0.73

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -16,6 +16,7 @@ enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB};
 
 size_t ramdisk_read(void *buf, size_t offset, size_t len);
 size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+size_t serial_write(const void *buf, size_t offset, size_t len);
 
 size_t invalid_read(void *buf, size_t offset, size_t len) {
   panic("should not reach here");
@@ -30,8 +31,8 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
   [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write, 0},
-  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, invalid_write, 0},
-  [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write, 0},
+  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, serial_write, 0},
+  [FD_STDERR] = {"stderr", 0, 0, invalid_read, serial_write, 0},
 #include "files.h"
 };
 
@@ -78,16 +79,7 @@ size_t fs_read(int fd, void *buf, size_t len){
 
 size_t fs_write(int fd, const void *buf, size_t len){
   long i = -1;
-  if(fd == FD_STDOUT || fd == FD_STDERR){
-    for(i = 0; i < len; i ++){
-      putch(*(char *)buf);
-      buf++;
-    }
-  }
-  else if(fd == FD_STDIN){
-    i = 0;
-  }
-  else if(fd >= 3 && fd < fs_size){
+  if(file_table[fd].write == NULL){
     if(len + file_table[fd].open_offset <= file_table[fd].size){
       i = ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, len);
       file_table[fd].open_offset += i;
@@ -95,6 +87,9 @@ size_t fs_write(int fd, const void *buf, size_t len){
     else{
       assert(0);
     }
+  }
+  else{
+    (*file_table[fd].write)(buf, 0, len);
   }
   return i;
 }
