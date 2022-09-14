cb4c09cb543d20bdb4354f9f6861a3a87dfa66dd
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-14 15:39:40 +0800

    > compile NEMU ysyx_22041461 陈星宇 Linux cxy-lenovo 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:39:40 up  4:19,  1 user,  load average: 0.68, 1.31, 1.34

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -23,7 +23,7 @@ int NDL_PollEvent(char *buf, int len) {
   // 读出一条事件信息, 将其写入`buf`中, 最长写入`len`字节
   // 若读出了有效的事件, 函数返回1, 否则返回0
   int fd = open("/dev/events", O_RDONLY);
-  if(read(fd, buf, len) > 0){
+  if(read(fd, buf, len) >= 0){
     close(fd);
     return 1;
   }
@@ -31,7 +31,6 @@ int NDL_PollEvent(char *buf, int len) {
     close(fd);
     return 0;
   }
-  close(fd);
 }
 
 void NDL_OpenCanvas(int *w, int *h) {
