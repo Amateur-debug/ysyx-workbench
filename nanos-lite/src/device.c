56888b8ee1b494aa91acbbc9077b3591cc262429
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-14 16:41:09 +0800

    > compile NEMU ysyx_22041461 陈星宇 Linux cxy-lenovo 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:41:09 up  5:20,  1 user,  load average: 2.61, 1.94, 1.46

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -26,7 +26,8 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 size_t events_read(void *buf, size_t offset, size_t len) {
   size_t ret = 0;
   AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
-  char *out = NULL;
+  char _out[len];
+  char *out = _out;
   if(ev.keycode == AM_KEY_NONE){
     return 0;
   }
