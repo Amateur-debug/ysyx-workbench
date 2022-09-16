2f015ceabf7780819c4658f515d448fc65a01efe
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-16 18:09:26 +0800

    > compile NEMU ysyx_22041461 陈星宇 Linux cxy-lenovo 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  18:09:26 up 21:27,  1 user,  load average: 0.78, 1.21, 1.48

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -68,7 +68,9 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
     else{
       x = 0;
       y++;
-      assert(y < screen_h);
+      if(y >= screen_h){
+        break;
+      }
       io_write(AM_GPU_FBDRAW, x , y, (void *)buf, 1, 1, false);
     }
     x++;
