d255b74f4be16c2c55fa11929c4ac4b2fd8afde0
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-17 11:35:34 +0800

    > compile NEMU ysyx_22041461 陈星宇 Linux cxy-lenovo 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  11:35:34 up 1 day, 14:53,  1 user,  load average: 1.86, 1.81, 1.56

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -72,6 +72,7 @@ void NDL_DrawRect(uint32_t *pixels, int x, int y, int w, int h) {
   printf("screen_w = %d screen_h = %d\n", screen_w, screen_h);
   memset(color_buf, 0, sizeof(uint32_t) * screen_w * screen_h);
   printf("screen_w = %d screen_h = %d\n", screen_w, screen_h);
+  printf("screen_w = %d screen_h = %d\n", screen_w, screen_h);
   int fd = open("/dev/fb", O_WRONLY);
   int i;
   int offset;
