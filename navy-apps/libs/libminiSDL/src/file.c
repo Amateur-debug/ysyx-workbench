91d555b3c77fb849fe8920121d932a1285ed291a
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-19 20:09:08 +0800

    > compile NEMU ysyx_22041461 陈星宇 Linux cxy-lenovo 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  20:09:08 up 1 day,  8:22,  1 user,  load average: 1.66, 1.54, 1.49

--- navy-apps/libs/libminiSDL/src/file.c
+++ navy-apps/libs/libminiSDL/src/file.c
@@ -1,11 +1,11 @@
 #include <sdl-file.h>
 
 SDL_RWops* SDL_RWFromFile(const char *filename, const char *mode) {
-  printf("SDL_RWFromFile\n");
+  //printf("SDL_RWFromFile\n");
   return NULL;
 }
 
 SDL_RWops* SDL_RWFromMem(void *mem, int size) {
-  printf("SDL_RWFromMem\n");
+  //printf("SDL_RWFromMem\n");
   return NULL;
 }
