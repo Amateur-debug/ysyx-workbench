00643ae3dfacf37036eeb6e8daf619bdaa55779c
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-19 16:33:50 +0800

    > compile NEMU ysyx_22041461 陈星宇 Linux cxy-lenovo 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:33:50 up 1 day,  4:46,  1 user,  load average: 1.57, 1.42, 1.35

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -96,7 +96,7 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
   NDL_OpenCanvas(&draw_w, &draw_h);
   if(s->format->palette != NULL){   //使用调色板
     uint32_t a[12] = {0};
-    printf("%d\n", sizeof(pixels));
+    printf("%d\n", sizeof(a));
     int i;
     for(i = 0; i < draw_w * draw_h; i++){
       uint8_t num = *(s->pixels + i);
