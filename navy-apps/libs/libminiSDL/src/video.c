e39545e995865dea915c510f561cab98d4724a4e
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-19 16:49:22 +0800

    > compile NEMU ysyx_22041461 陈星宇 Linux cxy-lenovo 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:49:22 up 1 day,  5:02,  1 user,  load average: 2.10, 1.58, 1.39

--- navy-apps/libs/libminiSDL/src/video.c
+++ navy-apps/libs/libminiSDL/src/video.c
@@ -95,16 +95,15 @@ void SDL_UpdateRect(SDL_Surface *s, int x, int y, int w, int h) {
   int draw_h = s->h;
   NDL_OpenCanvas(&draw_w, &draw_h);
   if(s->format->palette != NULL){   //使用调色板
-    uint32_t pixels[120000];
+    uint32_t pixels[12];
     printf("%d\n", sizeof(pixels));
     int i;
     for(i = 0; i < draw_w * draw_h; i++){
       uint8_t num = *(s->pixels + i);
       printf("下标 = %d\n", num);
-      SDL_Palette *palette = s->format->palette + num;
-      SDL_Color *color = palette->colors;
-      printf("aaa %d\n", color->val);
-      pixels[i] = color->val;
+      SDL_Palette palette = *(s->format->palette + num);
+      printf("aaa %d\n", palette.colors->val);
+      pixels[i] = palette.colors->val;
     }
     if(x == 0 && y == 0 && w == 0 && h == 0){
       NDL_DrawRect(pixels, 0, 0, draw_w, draw_h);
