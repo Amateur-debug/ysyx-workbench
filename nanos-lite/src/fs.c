fa1f191777ce9bcb358d56194ef4aac8181f8812
tracer-ysyx2204 <tracer@ysyx.org>
2022-09-20 14:38:06 +0800

    > compile NEMU ysyx_22041461 陈星宇 Linux cxy-lenovo 5.15.0-47-generic #51-Ubuntu SMP Thu Aug 11 07:51:15 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  14:38:06 up 2 days,  2:51,  1 user,  load average: 0.96, 1.56, 1.92

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -52,6 +52,7 @@ void init_fs() {
 }
 
 int fs_open(const char *pathname, int flags, int mode){
+  printf("%s\n", pathname);
   int i;
   for(i = 0; i < fs_size; i++){
     if(strcmp(pathname, file_table[i].name) == 0){
