55151711be7cd0045082ec08195a9aec174e0170
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-13 04:59:02 +0800

    > compile NEMU ysyx_22041461 陈星宇 Linux cxy-computer 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  04:59:02 up  9:33,  1 user,  load average: 0.35, 0.35, 0.36

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -14,7 +14,8 @@ LDFLAGS   += --gc-sections -e _start
 override NEMUFLAGS += -l $(shell dirname $(IMAGE).elf)/nemu-log.txt
 override NEMUFLAGS += -b
 override NEMUFLAGS += -m $(shell dirname $(IMAGE).elf)/nemu-mtrace.txt
-override NEMUFLAGS += -f $(shell dirname $(IMAGE).elf)/nemu-ftrace.txt $(IMAGE).elf
+override NEMUFLAGS += -f $(shell dirname $(IMAGE).elf)/nemu-ftrace.txt
+override NEMUFLAGS += --elf $(IMAGE).elf
 
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
 CFLAGS += -I$(AM_HOME)/am/src/platform/nemu/include
--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -82,6 +82,7 @@ static int parse_args(int argc, char *argv[]) {
     {"help"     , no_argument      , NULL, 'h'},
     {"mtrace"   , required_argument, NULL, 'm'},
     {"ftrace"   , required_argument, NULL, 'f'},
+    {"elf"      , required_argument, NULL, '2'},
     {0          , 0                , NULL,  0 },
   };
   int o;
@@ -92,7 +93,8 @@ static int parse_args(int argc, char *argv[]) {
       case 'l': log_file = optarg; break;
       case 'd': diff_so_file = optarg; break;
       case 'm': mtrace_file = optarg; break;
-      case 'f': ftrace_file = optarg; elf_file = argv[optind]; break;
+      case 'f': ftrace_file = optarg; break;
+      case 2: elf_file = optarg; break;
       case 1: img_file = optarg; printf("img=%s\n", img_file); return 0;
       default:
         printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
