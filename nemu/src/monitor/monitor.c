8afb25215cba3d1ec6a28ed6d304a2e1102378fd
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-13 04:13:53 +0800

    > compile NEMU ysyx_22041461 陈星宇 Linux cxy-computer 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  04:13:53 up  8:48,  1 user,  load average: 0.29, 0.29, 0.26

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -85,7 +85,7 @@ static int parse_args(int argc, char *argv[]) {
     {0          , 0                , NULL,  0 },
   };
   int o;
-  while ( (o = getopt_long(argc, argv, "-bhf:m:l:d:p:", table, NULL)) != -1) {
+  while ( (o = getopt_long(argc, argv, "bhf:m:l:d:p:", table, NULL)) != -1) {
     printf("o=%d\n", o);
     switch (o) {
       case 'b': sdb_set_batch_mode(); break;
