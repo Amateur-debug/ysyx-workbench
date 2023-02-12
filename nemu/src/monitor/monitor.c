e8fe2d3665cd2034ca4ea49b1040d1fb8e8e8ece
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-13 04:49:36 +0800

    > compile NEMU ysyx_22041461 陈星宇 Linux cxy-computer 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  04:49:36 up  9:24,  1 user,  load average: 0.30, 0.34, 0.35

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -94,8 +94,8 @@ static int parse_args(int argc, char *argv[]) {
       case 'l': log_file = optarg; break;
       case 'd': diff_so_file = optarg; break;
       case 'm': mtrace_file = optarg; break;
-      case 'f': ftrace_file = optarg; elf_file = argv[optind + 1]; break;
-      case 1: img_file = optarg; printf("%s\n", img_file); return 0;
+      case 'f': ftrace_file = optarg; elf_file = argv[optind + 1]; printf("%s\n", elf_file); break;
+      case 1: img_file = optarg; return 0;
       default:
         printf("Usage: %s [OPTION...] IMAGE [args]\n\n", argv[0]);
         printf("\t-b,--batch              run with batch mode\n");
