be5c22ddd8439e185f461331cc3eadd5ffd58fa3
tracer-ysyx2204 <tracer@ysyx.org>
2023-02-13 04:40:05 +0800

    > compile NEMU ysyx_22041461 陈星宇 Linux cxy-computer 5.15.0-43-generic #46-Ubuntu SMP Tue Jul 12 10:30:17 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  04:40:05 up  9:14,  1 user,  load average: 0.32, 0.30, 0.33

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -82,8 +82,7 @@ static int parse_args(int argc, char *argv[]) {
     {"help"     , no_argument      , NULL, 'h'},
     {"mtrace"   , required_argument, NULL, 'm'},
     {"ftrace"   , required_argument, NULL, 'f'},
-    {0          , 0                , NULL,  0 },
-    {0          , 0                , NULL,  0 },
+
   };
   int o;
   while ( (o = getopt_long(argc, argv, "-bhf:m:l:d:p:", table, NULL)) != -1) {
